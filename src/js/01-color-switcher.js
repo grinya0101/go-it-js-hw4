a2cce816809b48053178054149a6fb4fcdfbb8f4
grinya0101 <grinya1994015@gmail.com>
2023-02-04 05:25:03 +0300

    add2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+
+buttonStart.addEventListener('click', onBtnStart);
+buttonStop.addEventListener('click', onBtnStop);
+
+let intervalId = null;
+let isActive = false;
+
+function onBtnStart() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
